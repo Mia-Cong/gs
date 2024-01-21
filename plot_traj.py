c39d283f5fbf133151f8f15610ac8ecfa06baefa
Codey Sun <codey.sun@gmail.com>
2024-01-21 02:27:15 -0600

    Added tracking demo

---
+++ plot_traj.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({"font.size": 16, "axes.titlesize": 16, "axes.labelsize": 16})
+# plt.style.use('dark_background')
+
+
+def rmse(truth, predicted):
+    if truth.shape != predicted.shape:
+        raise ValueError("Input arrays must have same shape")
+
+    error = np.linalg.norm(truth - predicted, axis=1)
+
+    squared_diff = (error) ** 2
+    mse = np.mean(squared_diff)
+    root_mse = np.sqrt(mse)
+
+    return root_mse
+
+
+def multiply_quaternions(q1, q2):
+    w1 = q1[:, 0]
+    x1 = q1[:, 1]
+    y1 = q1[:, 2]
+    z1 = q1[:, 3]
+    w2 = q2[:, 0]
+    x2 = q2[:, 1]
+    y2 = q2[:, 2]
+    z2 = q2[:, 3]
+
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+
+    return np.array([w, x, y, z]).transpose()
+
+
+if __name__ == "__main__":
+    path = "output/room0/"
+    traj_track = np.load(path + "tracking_traj_100it_001lr_constvel.npy")
+    traj_gt = np.load(path + "tracking_traj_gt.npy")
+
+    fig1 = plt.figure(figsize=(6.4 * 1.5, 4.8 * 1.5))
+    ax1 = plt.axes()
+    ax1 = fig1.add_subplot(1, 1, 1, projection="3d")
+
+    ax1.set_xlabel("Meters (m)")
+    ax1.set_title("Tracking Trajectory")
+
+    fig1.set_facecolor("white")
+    ax1.set_facecolor("white")
+    ax1.xaxis.label.set_color("black")
+    ax1.tick_params(axis="x", colors="black")
+    ax1.yaxis.label.set_color("black")
+    ax1.tick_params(axis="y", colors="black")
+    fig1.set_tight_layout(True)
+    # ax1.grid(False)
+
+    print(traj_gt.shape)
+    print(traj_track.shape)
+
+    N = 1000
+
+    ax1.plot(
+        traj_gt[:N, 4],
+        traj_gt[:N, 5],
+        traj_gt[:N, 6],
+        color="red",
+        label="Ground truth",
+    )
+    ax1.plot(
+        traj_track[:N, 4],
+        traj_track[:N, 5],
+        traj_track[:N, 6],
+        color="blue",
+        label="Tracked",
+    )
+    ax1.legend()
+
+    fig1.tight_layout()
+
+    fig1.savefig("tracking.png", bbox_inches="tight", pad_inches=0.01, dpi=150)
+
+    # TODO: plot absolute trajectory error
+    fig2 = plt.figure(2)
+    ax2 = plt.axes()
+    ax2.set_xlabel("Frame")
+    ax2.set_ylabel("Meters (m)")
+    ax2.set_title("Absolute Trajectory Error")
+    ax2.plot(np.linalg.norm(traj_track[:N, 4:] - traj_gt[:N, 4:], axis=1))
+
+    # TODO: plot orientation error
+    fig3 = plt.figure(3)
+    ax3 = plt.axes()
+    ax3.set_xlabel("Frame")
+    ax3.set_ylabel("Angle (deg)")
+    ax3.set_title("Orientation Error")
+    gtconj = np.array(
+        [traj_gt[:N, 0], -traj_gt[:N, 1], -traj_gt[:N, 2], -traj_gt[:N, 3]]
+    ).transpose()
+    diff = multiply_quaternions(traj_track[:N, :4], gtconj)
+    diff = diff / np.expand_dims(np.linalg.norm(diff, axis=1), axis=1)
+    o_err = 2 * np.arccos(abs(diff[:N, 0]))
+    o_err_deg = np.rad2deg(o_err)  # Convert to degrees
+    ax3.plot(o_err_deg)
+    # Return rotation angle of the difference quaternion
+    # diff = diff / torch.norm(diff)
+    # return 2 * torch.acos(abs(diff[0]))
+
+    # Print RMSE
+    print(rmse(traj_gt[:, 4:], traj_track[:, 4:]))
+
+    plt.show()
---
+++ tracking_demo.py
@@ -0,0 +1,247 @@
+import torch
+from scene import Scene
+import os
+import sys
+from tqdm import tqdm
+from os import makedirs
+from gaussian_renderer import render
+import torchvision
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams, OptimizationParams, get_combined_args
+from gaussian_renderer import GaussianModel
+from utils.pose_utils import (
+    get_camera_from_tensor,
+    get_tensor_from_camera,
+    quadmultiply,
+)
+from utils.loss_utils import l1_loss, l2_loss, ssim, quat_loss
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+
+def prune_gaussians(gaussians, opac_thres):
+    # Prune Gaussians that have opacity below opac_thres
+    mask = gaussians.get_opacity > opac_thres
+    attributes = [
+        "_xyz",
+        "_scaling",
+        "_rotation",
+        "_opacity",
+        "_features_dc",
+        "_features_rest",
+    ]
+    for attr in attributes:
+        a = getattr(gaussians, attr)
+        setattr(
+            gaussians,
+            attr,
+            a[torch.squeeze(mask)],
+        )
+
+
+def optimize_cam(
+    opt,
+    view,
+    gaussians,
+    pipeline,
+    background,
+    optimizer,
+    camera_tensor_q,
+    camera_tensor_T,
+) -> float:
+    # We can keep camera orientation fixed, and then track a rigid body transformation
+    # of the Gaussians around the camera
+
+    # Apply w2c transform to gaussians
+    rel_w2c = get_camera_from_tensor(torch.cat([camera_tensor_q, camera_tensor_T]))
+    gaussians_xyz = gaussians._xyz.clone().detach()
+    gaussians_rot = gaussians._rotation.clone().detach()
+
+    pts_ones = torch.ones(gaussians_xyz.shape[0], 1).cuda().float()
+    pts4 = torch.cat((gaussians_xyz, pts_ones), dim=1)
+    gaussians_xyz_trans = (rel_w2c @ pts4.T).T[:, :3]
+    gaussians_rot_trans = quadmultiply(camera_tensor_q, gaussians_rot)
+    gaussians._xyz = gaussians_xyz_trans
+    gaussians._rotation = gaussians_rot_trans
+
+    # Render
+    result = render(
+        view,
+        gaussians,
+        pipeline,
+        background,
+    )
+    image = result["render"]
+
+    # Loss
+    gt_image = view.original_image.cuda()
+    loss = l1_loss(image, gt_image)
+    loss.backward()
+
+    # Optimize
+    with torch.no_grad():
+        optimizer.step()
+        optimizer.zero_grad(set_to_none=True)
+
+        # Restore untransformed points
+        gaussians._xyz = gaussians_xyz
+        gaussians._rotation = gaussians_rot
+
+    return loss, image
+
+
+def track(dataset, opt, pp, checkpoint_iter: int, const_velocity, clip_grad):
+    gaussians = GaussianModel(dataset.sh_degree, freeze_map=True)
+
+    scene = Scene(dataset, gaussians, load_iteration=checkpoint_iter, shuffle=False)
+    prune_gaussians(gaussians, 0.5)
+
+    gt_viewpoints_list = scene.getTrainCameras().copy()
+
+    # Load GT poses
+    gt_camera_tensor_list = []
+    for viewpoint in gt_viewpoints_list:
+        # Convert W2C transformation matrix to a quaternion + translation vector
+        w2c = viewpoint.world_view_transform.transpose(0, 1)
+        camera_tensor = get_tensor_from_camera(w2c)
+        gt_camera_tensor_list.append(camera_tensor)
+
+    # Create saved list of all camera poses
+    camera_tensor_list = torch.zeros([len(gt_viewpoints_list), 7]).cuda()
+    pos_np = np.zeros([len(gt_viewpoints_list), 7])
+    pos_np_init = np.zeros([len(gt_viewpoints_list), 7])
+    pos_np_gt = np.zeros([len(gt_viewpoints_list), 7])
+
+    # Get the first camera tensor as the starting point
+    camera_tensor = gt_camera_tensor_list[0]
+    camera_tensor_T = camera_tensor[-3:].requires_grad_()
+    camera_tensor_q = camera_tensor[:4].requires_grad_()
+
+    bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
+    background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+    progress_bar = tqdm(gt_viewpoints_list, desc="Tracking progress")
+
+    # Save renders for reference
+    render_path = os.path.join(dataset.model_path, "tracking_renders")
+    makedirs(render_path, exist_ok=True)
+
+    # Pick the next camera
+    for idx, view in enumerate(progress_bar):
+        # Stop prematurely at specified idx
+        # if idx == 300:
+        #     break
+
+        # # If using the constant velocity model
+        # if const_velocity and idx - 2 >= 0:
+        #     pre_w2c = get_camera_from_tensor(camera_tensor)
+        #     delta = (
+        #         pre_w2c @ get_camera_from_tensor(camera_tensor_list[idx - 2]).inverse()
+        #     )
+        #     camera_tensor = get_tensor_from_camera(delta @ pre_w2c)
+        #     # camera_tensor.requires_grad_()
+        #     camera_tensor_T = camera_tensor[-3:].requires_grad_()
+        #     camera_tensor_q = camera_tensor[:4].requires_grad_()
+
+        pose_optimizer = torch.optim.Adam(
+            [
+                {"params": [camera_tensor_T], "lr": 0.01},
+                {"params": [camera_tensor_q], "lr": 0.001},
+            ]
+        )
+
+        with torch.no_grad():
+            pos_np_init[idx, :] = (
+                torch.cat([camera_tensor_q, camera_tensor_T]).cpu().detach().numpy()
+            )
+
+        # For some iterations
+        for cam_iter in range(100):
+            loss, rendering = optimize_cam(
+                opt,
+                view,
+                gaussians,
+                pp,
+                background,
+                pose_optimizer,
+                camera_tensor_q,
+                camera_tensor_T,
+            )
+
+        with torch.no_grad():
+            # Save images
+            torchvision.utils.save_image(
+                torch.cat(
+                    [
+                        view.original_image[0:3, :, :],
+                        rendering,
+                        torch.abs(rendering - view.original_image[0:3, :, :]),
+                    ],
+                    dim=2,
+                ),
+                os.path.join(render_path, "{0:05d}".format(idx) + ".png"),
+            )
+
+            progress_bar.set_postfix({"Loss Diff": f"{initial_loss:.4f}->{loss:.4f}"})
+            progress_bar.update()
+
+            camera_tensor_list[idx] = (
+                torch.cat([camera_tensor_q, camera_tensor_T]).clone().detach()
+            )
+            pos_np[idx, :] = camera_tensor_list[idx].cpu().detach().numpy()
+            pos_np_gt[idx, :] = gt_camera_tensor_list[idx].cpu().detach().numpy()
+
+    # If we ended early, delete zero rows
+    non_zero_rows_mask = np.any(pos_np != 0, axis=1)
+    pos_np = pos_np[non_zero_rows_mask]
+    pos_np_init = pos_np_init[non_zero_rows_mask]
+    pos_np_gt = pos_np_gt[non_zero_rows_mask]
+
+    np.save(scene.model_path + "/tracking_traj", pos_np, allow_pickle=True)
+    np.save(scene.model_path + "/tracking_traj_init", pos_np_init, allow_pickle=True)
+    np.save(scene.model_path + "/tracking_traj_gt", pos_np_gt, allow_pickle=True)
+
+    progress_bar.close()
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1, projection="3d")
+    ax1.plot(pos_np[:, 4], pos_np[:, 5], pos_np[:, 6], label="optim")
+    ax1.plot(pos_np_gt[:, 4], pos_np_gt[:, 5], pos_np_gt[:, 6], label="gt")
+    ax1.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Tracking script parameters")
+    model = ModelParams(parser, sentinel=True)
+    op = OptimizationParams(parser)
+    pipeline = PipelineParams(parser)
+    parser.add_argument("--iteration", default=-1, type=int)
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--detect_anomaly", action="store_true", default=False)
+    parser.add_argument("--const_velocity", action="store_true", default=False)
+    parser.add_argument("--pose_noise", type=float, default=0.0)
+    parser.add_argument("--clip_grad", action="store_true", default=False)
+
+    # args = parser.parse_args(sys.argv[1:])
+    args = get_combined_args(parser)
+    print("Tracking " + args.model_path)
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    torch.autograd.set_detect_anomaly(args.detect_anomaly)
+    track(
+        model.extract(args),
+        op.extract(args),
+        pipeline.extract(args),
+        args.iteration,
+        args.const_velocity,
+        args.clip_grad,
+    )
+
+    print("\nTracking complete.")
